14100b18514c7148c977b23782ae3fe0b549951c
KuJaX <jose_punta@hotmail.com>
2014-03-30 10:56:36 +0200

    Create Resultado.java

---
+++ Resultado.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+
+public class Resultado {
+    private Faro faro;
+    private ArrayList<Double> indicesDeCorrectitud;
+    
+    public Resultado(Faro faro) {
+        this.faro = faro;
+        this.indicesDeCorrectitud = new ArrayList<Double>();
+    }
+    
+    public Faro getFaro() {
+        return faro;
+    }
+    
+    public void addIndice(double i) {
+        this.indicesDeCorrectitud.add(i);
+    }
+    
+    public ArrayList<Double> getIndices() {
+        return this.indicesDeCorrectitud;
+    }
+}
