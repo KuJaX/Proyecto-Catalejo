6c1f1b375e2b7b630d0f6c4a141b163f5ff74ba9
KuJaX <jose_punta@hotmail.com>
2014-04-24 13:20:38 +0200

    Update Metodos.java

--- Metodos.java
+++ Metodos.java
@@ -12,6 +12,7 @@ import java.util.logging.Logger;
 public class Metodos {
     
     private static final String mayusculas = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+
     public static String slug(String str) {
         return Normalizer.normalize(str, Normalizer.Form.NFD)
                 .replaceAll("[^\\p{ASCII}]", "")
@@ -23,7 +24,7 @@ public class Metodos {
     }
 
     public ArrayList<Resultado> leer(String nombre) {
-         FileReader lectorArchivo;
+        FileReader lectorArchivo;
         ArrayList<Resultado> lista = new ArrayList<Resultado>();
         try {
             lectorArchivo = new FileReader(new File(nombre));
@@ -47,14 +48,16 @@ public class Metodos {
                 int numeroDeDestellos = 0;
                 int n = 0;
                 int i = 0;
-                int t = 0;
+                int t;
                 int v = 0;
+                int k;
                 double tiempo_Luz = 0;
                 double tiempo_Ocultacion = 0;
 
                 System.out.println(linea);
 
                 while (n < linea.length()) {
+                    t = 0;
                     char caracter = linea.charAt(n);
                     char caracterSiguiente = '-';
                     try {
@@ -127,11 +130,12 @@ public class Metodos {
                             case  9:
                                 break;
                             case 10:
-                                char parte2;
+                                char cuenta;
                                 char punto = '.';
                                 String tiempoL = "";
                                 String tiempoO = "";
-                                int numero2;
+                                String cadena_cuenta;
+                                int numero_que_se_repite;
                                 luz = new double[50];
                                 ocultaciones = new double[50];
                                 //caracteristicas
@@ -139,19 +143,45 @@ public class Metodos {
                                     t++;
                                     n += 2;
                                     caracter = linea.charAt(n);
-                                            if(caracterSiguiente == ' ' && linea.charAt(n+2) != 'o'){
+                                            if(linea.charAt(n+8) == ')'){
+                                                cuenta = linea.charAt(n+9);
+                                                cadena_cuenta = "" + cuenta;
+                                                numero_que_se_repite = Integer.parseInt(cadena_cuenta);
+                                                
+                                                if(caracterSiguiente == ' ' && linea.charAt(n+2) != 'o'){
                                                 tiempoL += caracter;
                                                 tiempoL += punto;
                                                 tiempoL += linea.charAt(n+2);
                                                 tiempo_Luz = Double.parseDouble(tiempoL);
+                                                    for(k = 0;k < numero_que_se_repite;k++){
+                                                        luz[t] = tiempo_Luz;
+                                                        t++;
+                                                    }
+                                                }
+                                                else {
+                                                    tiempoL += caracter;
+                                                    tiempo_Luz = Double.parseDouble(tiempoL);
+                                                    for(k = 0;k < numero_que_se_repite;k++){
+                                                        luz[t] = tiempo_Luz;
+                                                        t++;
+                                                    }
+                                                }        
+                                            }
+                                            else if(caracterSiguiente == ' ' && linea.charAt(n+2) != 'o'){
+                                                tiempoL += caracter;
+                                                tiempoL += punto;
+                                                tiempoL += linea.charAt(n+2);
+                                                tiempo_Luz = Double.parseDouble(tiempoL);
+                                                luz[t] = tiempo_Luz;
                                             }
                                             else if(caracter == ' ' && caracterSiguiente == 'o') break;
                                             else {
                                                 tiempoL += caracter;
                                                 tiempo_Luz = Double.parseDouble(tiempoL);
+                                                luz[t] = tiempo_Luz;
                                             }
                                         
-                                        luz[t] = tiempo_Luz;
+                                        
                                     break;
                                     }
                                 else if (caracter == 'o'){
@@ -164,6 +194,7 @@ public class Metodos {
                                                 tiempoO += punto;
                                                 tiempoO += linea.charAt(n+2);
                                                 tiempo_Ocultacion = Double.parseDouble(tiempoO);
+                                                ocultaciones[v] = tiempo_Ocultacion;
 
                                             }
                                             else if(caracter == ' ' && caracterSiguiente == 'L' || 
@@ -171,8 +202,9 @@ public class Metodos {
                                             else {
                                                 tiempoO += caracter;
                                                 tiempo_Ocultacion = Double.parseDouble(tiempoO);
+                                                ocultaciones[v] = tiempo_Ocultacion;
                                             }
-                                      ocultaciones[v] = tiempo_Ocultacion;
+                                      
                                       break; 
                                 }
                                 else break;
@@ -182,12 +214,16 @@ public class Metodos {
                             case 12:
                             case 13:
                                 break;
+                            //case 13: break; (No sirve porque nunca entraria
                         }   
                     }
                     n++;
                     System.out.println(linea + " /// " + nombre_faro + ":"+ " " + longitud +" - "+ latitud + " - " +  coord_X + " - " +  coord_Y + " - " +  tiempo_Luz + " - " +  tiempo_Ocultacion + " - " + repeticiones + " - " + color + " - " + tiempoTotal);
                 }
-
+                System.out.println("Estas son los tiempos de Luz:");
+                System.out.println(luz[0]);
+                System.out.println("Estas son los tiempos de Ocultacion:");
+                System.out.println(ocultaciones[0]);
                 Faro f = new Faro(nombre_faro, longitud, latitud, coord_X, coord_Y, repeticiones, color, ocultaciones, luz, Double.parseDouble(tiempoTotal));
 		lista.add(new Resultado(f));
                 linea = br.readLine();
@@ -213,5 +249,3 @@ public class Metodos {
         return lista;
     }
 }
-
-
